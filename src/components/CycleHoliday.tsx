d21971505c5965ca740ee59cd6407a260cc65c3b
JaredThacker <pricesmayvary302@gmail.com>
2024-09-15 15:42:32 -0400

    advance by alphabet

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,7 +1,56 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+enum Holiday {
+    Christmas = "Christmas",
+    Halloween = "Halloween",
+    Thanksgiving = "Thanksgiving",
+    NewYears = "NewYears",
+    Valentines = "Valentines",
+}
+
+type HolidayEmojis = "🎄" | "🎃" | "🦃" | "🎆" | "💘";
+
+const HolidayRecord: Record<HolidayEmojis, Holiday> = {
+    "🎄": Holiday.Christmas,
+    "🎃": Holiday.Halloween,
+    "🦃": Holiday.Thanksgiving,
+    "🎆": Holiday.NewYears,
+    "💘": Holiday.Valentines,
+};
+
 export function CycleHoliday(): React.JSX.Element {
-    useState();
-    return <Button>Cycle Holiday</Button>;
+    const [holiday, setHoliday] = useState<Holiday>(Holiday.NewYears);
+
+    function alphabetical(): void {
+        if (holiday === HolidayRecord["🎄"]) {
+            setHoliday(HolidayRecord["🎃"]);
+            return;
+        } else if (holiday === HolidayRecord["🎃"]) {
+            setHoliday(HolidayRecord["🎆"]);
+            return;
+        } else if (holiday === HolidayRecord["🎆"]) {
+            setHoliday(HolidayRecord["🦃"]);
+            return;
+        } else if (holiday === HolidayRecord["🦃"]) {
+            setHoliday(HolidayRecord["💘"]);
+            return;
+        } else if (holiday === HolidayRecord["💘"]) {
+            setHoliday(HolidayRecord["🎄"]);
+            return;
+        }
+    }
+
+    return (
+        <>
+            <Button
+                onClick={() => {
+                    alphabetical();
+                }}
+            >
+                Advance by Alphabet
+            </Button>
+            <span>Holiday: {holiday}</span>
+        </>
+    );
 }
