4eca8c825e034406163941344049b248779924f0
JaredThacker <pricesmayvary302@gmail.com>
2024-09-21 16:08:28 -0400

    CheckAnswer form complete

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
+import { FormControl, FormGroup, FormLabel } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <FormGroup controlId="CheckAnswers">
+                <FormLabel>Check Answers</FormLabel>
+                <FormControl
+                    type="string"
+                    as={"textarea"}
+                    value={answer}
+                    onChange={updateAnswer}
+                />
+            </FormGroup>
+            <div>{expectedAnswer === answer ? "✔️" : "❌"}</div>
         </div>
     );
 }
